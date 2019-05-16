e02628a921ca71b4364d3a08496050852437df84
jinfull <jinfull@gmail.com>
2019-05-16 13:45:29 -0700

    Fix webpack issue for displaying chart from entry js file, minor CSS fix, add players.json file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-function component() {
-    const element = document.createElement('div');
+import { data } from './data';
 
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpdjkJLSDKLKDJDdjdack'], ' ');
+document.addEventListener('DOMContentLoaded', () => {
+    d3.select(document.getElementById('chart'))
+    .append("svg")
+    .chart("BasketballShotChart", {
+        width: 900,
+        title: 'Lebron Jokic 2013-14',
+        hexagonFillValue: function (d) { return d.z; },
+        // reverse the heat range to map our z values to other colors
+        heatScale: d3.scale.quantile()
+            .domain([-2.5, 2.5])
+            .range(['#5458A2', '#6689BB', '#FADC97', '#F08460', '#B02B48']),
+        hexagonBin: function (point, bin) {
+            var currentZ = bin.z || 0;
+            var totalAttempts = bin.attempts || 0;
+            var totalZ = currentZ * totalAttempts;
 
-    return element;
-}
-
-document.body.appendChild(component());
+            var attempts = point.attempts || 1;
+            bin.attempts = totalAttempts + attempts;
+            bin.z = (totalZ + (point.z * attempts)) / bin.attempts;
+        },
+        // update radius threshold to at least 4 shots to clean up the chart
+        hexagonRadiusThreshold: 2,
+    })
+    .draw(data);
+});
