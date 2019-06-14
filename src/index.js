d2f2efdf6b6bbce7a0021999283f5e4043f5eb2c
jinfull <jinfull@gmail.com>
2019-06-14 13:53:43 -0700

    Tweaks to intervals as well as adding nested setInterval

--- src/index.js
+++ src/index.js
@@ -5,14 +5,26 @@ const allPlayers = Object.keys(myData[0]).sort();
 
 let showChart = (data) => {
   const container = d3.select(".chart-container");
+
+
+  // console.log(data);
+  // container.style("opacity", "0");
+
+  // container
+  //   .data(data)
+  //   .transition().duration(750)
+  //   .style("opacity", "0")
+  //   .transition().duration(750)
+  //   .style("opacity", "1");
+
+  // var idTwo = setInterval(() => container.select('#chart').remove(),1000);
+
   container.select('#chart').remove();
+  
   container.append("div").attr("id", "chart")
     .append("svg")
     .chart("BasketballShotChart", {
       width: 800,
-      // title: (d) => {
-      //   return '';
-      // },
       hexagonFillValue: function (d) {
         return d.z;
       },
@@ -35,20 +47,11 @@ let showChart = (data) => {
     })
     .draw(data);    
 
-    var currHexagons = d3.selectAll(".shot-chart-hexagon");
-    // var nextHexagons;
 
-    // var myHexagons = d3.selectAll(".shot-chart-hexagon")
-    //   .data(data)
-    //   .transition().duration(1000)
-    //     .style("fill", function (d) { 
-    //     // console.log(d)
-    //     return (d.z); 
-    //   });
+    // Hexagon transitions fade-in and fade-out
+    var currHexagons = d3.selectAll(".shot-chart-hexagon");
 
     currHexagons.style("opacity", "0");
-
-
     currHexagons
       .data(data)
       .transition().duration(1000)
@@ -56,10 +59,13 @@ let showChart = (data) => {
       .style("fill", function (d) {
         return (d.z); 
       })
-      .transition().duration(300)
-      .style("opacity", ".75");
+      .transition().duration(500)
+      .style("opacity", "0.75");
+
+    var nextHexagons = d3.selectAll(".shot-chart-hexagon")
 
 
+    // Experiment with drawing a total new chart on top
     // nextHexagons
     //   .data(data2)
     //   .transition().duration(1000)
@@ -67,8 +73,82 @@ let showChart = (data) => {
     //   .style("fill", function (d) {
     //     return (d.z);
     //   });
-    
 };
+
+let showChartTwo = (data) => {
+  const container = d3.select(".chart-container");
+
+
+  console.log('This is showChartTwo argument data: ' + data);
+  container.style("opacity", "0");
+
+  container
+    .data(data)
+    .transition().duration(750)
+    .style("opacity", "0")
+    .transition().duration(750)
+    .style("opacity", "1");
+
+  // var idTwo = setInterval(() => container.select('#chart').remove(),1000);
+
+  container.select('#chart').remove();
+
+  container.append("div").attr("id", "chart")
+    .append("svg")
+    .chart("BasketballShotChart", {
+      width: 800,
+      hexagonFillValue: function (d) {
+        return d.z;
+      },
+      // reverse the heat range to map our z values to other colors
+      heatScale: d3.scale
+        .quantile()
+        .domain([-2.5, 2.5])
+        .range(["#5458A2", "#6689BB", "#FADC97", "#F08460", "#B02B48"]),
+      hexagonBin: function (point, bin) {
+        var currentZ = bin.z || 0;
+        var totalAttempts = bin.attempts || 0;
+        var totalZ = currentZ * totalAttempts;
+
+        var attempts = point.attempts || 1;
+        bin.attempts = totalAttempts + attempts;
+        bin.z = (totalZ + point.z * attempts) / bin.attempts;
+      },
+      // update radius threshold to at least 4 shots to clean up the chart
+      hexagonRadiusThreshold: 2
+    })
+    .draw(data);
+
+
+  // Hexagon transitions fade-in and fade-out
+  var currHexagons = d3.selectAll(".shot-chart-hexagon");
+
+  currHexagons.style("opacity", "0");
+  currHexagons
+    .data(data)
+    .transition().duration(1000)
+    .style("opacity", "1")
+    .style("fill", function (d) {
+      return (d.z);
+    })
+    .transition().duration(500)
+    .style("opacity", "0.75");
+
+
+
+
+  // Experiment with drawing a total new chart on top
+  // nextHexagons
+  //   .data(data2)
+  //   .transition().duration(1000)
+  //   .style("opacity", "0")
+  //   .style("fill", function (d) {
+  //     return (d.z);
+  //   });
+};
+
+
+
 
 
 
@@ -144,18 +224,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
     id = setInterval(() => {
       if (rangeSlider.value < rangeSlider.max) {
-        rangeSlider.value++;
         const currPlayer = playerDropdown.property('value');
         showChart(myData[0][currPlayer][rangeSlider.value]);
+        rangeSlider.value++;
         rangeBullet.innerHTML = rangeSlider.value;
         showSliderValue();
+
+        setInterval(() => {
+          showChartTwo(myData[0][currPlayer][rangeSlider.value++]);
+        }, 500);
       }
     }, 1500);
   });
 
   playerDropdown.property('value', randPlayer);
   rangeBullet.innerHTML = minYear;
+
+
+
+
   showChart(myData[0][randPlayer][minYear]);
+  // showChartTwo(myData[0][randPlayer][maxYear]);
+
+  setInterval(() => {
+
+  })
 
   slider.attr('min', minYear)
     .attr('max', maxYear)
@@ -185,28 +278,28 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   function search(playerName) {
-    /* select the input element */
-    let playerImgDiv = d3.select('#playerImg');
+    // /* select the input element */
+    // let playerImgDiv = d3.select('#playerImg');
 
-    // /* fetch the following URL that includes apikey, cx and the value of input */
-    fetch(`https://www.googleapis.com/customsearch/v1/siterestrict?key=${apiKey}&cx=${cx}&q=${playerName}`).then(response => response.text()).then(text => {
-      let result = JSON.parse(text);
+    // // /* fetch the following URL that includes apikey, cx and the value of input */
+    // fetch(`https://www.googleapis.com/customsearch/v1/siterestrict?key=${apiKey}&cx=${cx}&q=${playerName}`).then(response => response.text()).then(text => {
+    //   let result = JSON.parse(text);
 
-      if (!result.items) {
-        fetch(`https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${playerName}`).then(response => response.text()).then(text => {
-          result = JSON.parse(text);
-        });
-      }
+    //   if (!result.items) {
+    //     fetch(`https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${playerName}`).then(response => response.text()).then(text => {
+    //       result = JSON.parse(text);
+    //     });
+    //   }
 
-      if (!result.items) {
-        playerImgDiv.attr("src", "assets/stern.jpg");
-        return;
-      }
+    //   if (!result.items) {
+    //     playerImgDiv.attr("src", "assets/stern.jpg");
+    //     return;
+    //   }
 
-      let myImageSrc = result.items[0].pagemap.cse_image[0].src;
+    //   let myImageSrc = result.items[0].pagemap.cse_image[0].src;
 
-      playerImgDiv.attr("src", myImageSrc);
-    });
+    //   playerImgDiv.attr("src", myImageSrc);
+    // });
   }
 
   search(randPlayer);
